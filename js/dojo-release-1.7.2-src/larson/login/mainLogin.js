c9c0e1e93ad54fe6e034140f89792668d698c464
Greg Davis <antelope.buzkowksi@gmail.com>
2012-04-27 00:40:29 -0600

    Getting the widget to talk to couchdb and validate

--- js/dojo-release-1.7.2-src/larson/login/mainLogin.js
+++ js/dojo-release-1.7.2-src/larson/login/mainLogin.js
@@ -4,13 +4,55 @@ define([
 	"dijit/_Widget",
 	"dijit/_Templated", 
 	"dojo/_base/declare",
+	"dojo/_base/connect",
 	"dijit/form/Button",
+	"dijit/form/ValidationTextBox",
 	"dojo/text!./resources/mainLogin.html"
-    ], function(_Widget, _Templated, declare, Button, template) {
+    ], function(_Widget, _Templated, declare, connect, Button, TextBox, template) {
    
     return declare("larson.login.mainLogin", [_Widget, _Templated], {
         templateString: template,
         widgetsInTemplate: true,
-        baseClass: "mainLogin"
+        baseClass: "mainLogin",
+        postCreate: function() {
+        	this.inherited(arguments)
+        	
+        	var usernameField = dijit.byId("username"),
+        		loginButton = dijit.byId("loginButton")
+        	
+        	connect.connect(usernameField, "onChange", this.getData)
+        	connect.connect(loginButton, "onClick", this.validatePassword)
+        },
+        password: "",
+        getData: function() {
+        	this.inherited(arguments)
+        	var username = dijit.byId("username").get("value")
+			var xhrArgs = {
+				url: "http://localhost/users/"+username,
+				handleAs: "json",
+				load: function(data) { 
+					dojo.byId("usernameError").innerHTML = ""
+					this.password = data.password
+				},
+				error: function(error) {
+					dojo.byId("usernameError").innerHTML = "Username not found"
+				}
+			}
+			
+			var deferred = dojo.xhrGet(xhrArgs);
+        },
+        validatePassword: function() {
+        	this.inherited(arguments)
+        	var passwordEntered = dijit.byId("password").get("value")
+        	alert(passwordEntered + " " + this.password)
+        	
+        	if(passwordEntered == this.password) {
+        		window.location="/page2.html"
+				dojo.byId("passwordError").innerHTML = ""
+        	}
+        	else {
+        		dojo.byId("passwordError").innerHTML = "You entered the wrong password"
+        	}
+        }
     })
 })
